0de23b0caa65122cc5bf23eaf4620f609d19a9c7
Eric Gadzi <78027399+Eric-Gadzi@users.noreply.github.com>
2022-11-24 03:13:56 +0000

    fixing add to item

--- actions/cart_action/add_item_cart.php
+++ actions/cart_action/add_item_cart.php
@@ -1,47 +1,70 @@
-<?php 
+<?php
 
-    require_once dirname(__FILE__)."/../../controllers/cart_controller.php";
+require_once dirname(__FILE__) . "/../../controllers/cart_controller.php";
 
-    $item_id = $_GET['item_id'];
-    $item_qty = 1;
-    $item_cat = $_GET['cat'];
-    $ip_address = $_SERVER['REMOTE_ADDR'];
-    
-    echo $item_cat;
-    echo $item_id;
-    echo $ip_address;
-    echo $item_qty;
+$item_id = $_GET['item_id'];
+$item_qty = 1;
+$item_cat = $_GET['cat'];
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
+echo $item_cat;
+echo $item_id;
+echo $ip_address;
+echo $item_qty;
 
 
-    session_start();
-    
-    $user_id = $_SESSION['user_id'];
+session_start();
 
-    echo "<br>user_id: ".$user_id;
+$user_id = $_SESSION['user_id'];
 
+echo "<br>user_id: " . $user_id;
+
+
+
+
+if ($item_cat == 1) {
     $result1 = determine_item_in_cart_ctr($user_id, $item_id);
+    if ($result1) {
+        header("location:./../../view/transport.php");
+        return;
+    } else {
+        $result = addToItemsCart_ctr($item_id, $ip_address, $user_id, $item_qty);
 
-
-    if($result1){
-        if($item_cat == 1){
+        if ($result) {
+            echo "<script>alert('success')</script>";
             header("location:./../../view/transport.php");
-            return;
-        }else if($item_cat == 2){
+        }
+    }
+} 
+
+
+
+else if ($item_cat == 2) {
+    $result1 = determine_item_in_cart_ctr($user_id, $item_id);
+    if ($result1) {
+        header("location:./../../view/Subtotal.php");
+        return;
+    }else{
+        $result = addToItemsCart_ctr($item_id, $ip_address, $user_id, $item_qty);
+
+        if ($result) {
+            echo "<script>alert('success')</script>";
             header("location:./../../view/Subtotal.php");
             return;
         }
+    }
+    // header("location:./../../view/Subtotal.php");
     
-        
-    }
-
-    $result = addToItemsCart_ctr($item_id,$ip_address,$user_id,$item_qty);
-
-    if($result){
-        echo "<script>alert('success')</script>";
-        header("location:./../../view/transport.php");
-    }else{
-        echo "<script>alert('failed')</script>";
-    }
+}
 
 
-?>
+
+
+// $result = addToItemsCart_ctr($item_id, $ip_address, $user_id, $item_qty);
+
+// if ($result) {
+//     echo "<script>alert('success')</script>";
+//     header("location:./../../view/transport.php");
+// } else {
+//     echo "<script>alert('failed')</script>";
+// }

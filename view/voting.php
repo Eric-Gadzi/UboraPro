80e91ca7f7698eb943f8cf8b3beba987f3b0d602
Lindalab <linda.arthur@ashesi.edu.gh>
2022-11-24 01:35:40 +0000

    Vote nav bar

--- view/voting.php
+++ view/voting.php
@@ -53,6 +53,12 @@ require_once("../functions/awards_functions.php");
           </a>
         </div>
         <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.php" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Tickets.php" style="padding: 10px 20px;">Tickets</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="voting.php" style="padding: 10px 20px;">Voting</a>
+</li></ul>
+          </div>      
+        <div class="u-custom-menu u-nav-container">
           <ul class="u-nav u-unstyled u-nav-1">
             <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.php" style="padding: 10px 20px;">Home</a>
             </li>

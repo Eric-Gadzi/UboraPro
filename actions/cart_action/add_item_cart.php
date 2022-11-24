c17f3988fbd87c7d5f6f40ce47df3d27916b8667
Eric Gadzi <78027399+Eric-Gadzi@users.noreply.github.com>
2022-11-24 02:40:15 +0000

    fixing add to item

--- actions/cart_action/add_item_cart.php
+++ actions/cart_action/add_item_cart.php
@@ -1,40 +1,41 @@
 <?php 
-    require_once dirname(__FILE__)."/../../controllers/cart_controller.php";
+    echo  "linda is the sweetest";
+    // require_once dirname(__FILE__)."/../../controllers/cart_controller.php";
 
-    $item_id = $_GET['item_id'];
-    $item_qty = 1;
-    $item_cat = $_GET['cat'];
+    // $item_id = $_GET['item_id'];
+    // $item_qty = 1;
+    // $item_cat = $_GET['cat'];
   
  
-    $ip_address = $_SERVER['REMOTE_ADDR'];
+    // $ip_address = $_SERVER['REMOTE_ADDR'];
     
 
 
-    if(!isset($_SESSION['user_id'])){
-        session_start();
-    }
-    $user_id = $_SESSION['user_id'];
+    // if(!isset($_SESSION['user_id'])){
+    //     session_start();
+    // }
+    // $user_id = $_SESSION['user_id'];
 
-    if(determine_item_in_cart_ctr($user_id,$ip_address, $item_id)){
-        if($item_cat == 1){
-            header("location:./../../view/transport.php");
-            return;
-        }else if($item_cat == 2){
-            header("location:./../../view/Subtotal.php");
-            return;
-        }
+    // if(determine_item_in_cart_ctr($user_id,$ip_address, $item_id)){
+    //     if($item_cat == 1){
+    //         header("location:./../../view/transport.php");
+    //         return;
+    //     }else if($item_cat == 2){
+    //         header("location:./../../view/Subtotal.php");
+    //         return;
+    //     }
       
         
-    }
+    // }
 
-    $result = addToItemsCart_ctr($item_id,$ip_address,$user_id,$item_qty);
+    // $result = addToItemsCart_ctr($item_id,$ip_address,$user_id,$item_qty);
 
-    if($result){
-        echo "<script>alert('success')</script>";
-        header("location:./../../view/transport.php");
-    }else{
-        echo "<script>alert('failed')</script>";
-    }
+    // if($result){
+    //     echo "<script>alert('success')</script>";
+    //     header("location:./../../view/transport.php");
+    // }else{
+    //     echo "<script>alert('failed')</script>";
+    // }
 
 
 ?>
